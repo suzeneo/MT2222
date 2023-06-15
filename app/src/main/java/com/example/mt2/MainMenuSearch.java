e3b40fd467363d094b61d4d1d9b96a04a9fec467
82105 <yyy4735@naver.com>
2023-06-15 16:19:59 +0900

    dmd?

--- app/src/main/java/com/example/mt2/MainMenuSearch.java
+++ app/src/main/java/com/example/mt2/MainMenuSearch.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class MainMenuSearch extends Fragment {
@@ -27,6 +28,13 @@ public class MainMenuSearch extends Fragment {
             @Override
             public void onClick(View v) {
 
+                    Bundle bundle = new Bundle(); //무언가 담을 준비를 할 수 있는 보따리
+                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+                    fragment_shape_result fragment_shape_result = new fragment_shape_result();
+                    fragment_shape_result.setArguments(bundle);
+                    transaction.replace(R.id.menu_frame_layout,fragment_shape_result);
+                    transaction.commit(); //저장
             }
         });
 
--- app/src/main/java/com/example/mt2/fragment_shape_result.java
+++ app/src/main/java/com/example/mt2/fragment_shape_result.java
@@ -4,60 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_shape_result#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class fragment_shape_result extends Fragment {
+    TextView textview;
+    ListView listView;
+    View view;
+    FillInfoArrayList fillIfoArrayList = new FillInfoArrayList();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public fragment_shape_result() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_shape_result.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_shape_result newInstance(String param1, String param2) {
-        fragment_shape_result fragment = new fragment_shape_result();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shape_result, container, false);
+
+        view = inflater.inflate(R.layout.fragment_shape_result, container, false);
+
+        ArrayList<FillInfo> arrayList = fillIfoArrayList.getArrayList();
+        listView = (ListView) view.findViewById(R.id.listView);
+        final FillInfoAdaptor.FillInfoAdapter adapter = new FillInfoAdaptor.FillInfoAdapter(getActivity().getApplicationContext(), arrayList);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                Toast.makeText(getActivity().getApplicationContext(), adapter.getItem(position).getItemName()+"", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 }
